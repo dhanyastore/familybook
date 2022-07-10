25846c46e43c0f9eee8f96746ae0e0886376e197
dhanyastore <103267737+dhanyastore@users.noreply.github.com>
2022-07-10 16:49:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+
+
+
+var i =0;
+
+
+
+function update()
+{
+    if(i>5)
+    {
+      i=0;
+    }
+
+    if(i<=5)
+    {
+      document.getElementById("family_member_image").src = images[i];
+      document.getElementById("family_member_name").innerHTML = names[i];
+    }
+    i=i+1;
+
+    }
+      
+
